8be5d65854b21aa90df10ea749c7c018888b04b1
Rimzan Sadikeen <rimzansadikeen@ymail.com>
2017-12-11 17:44:19 +0530

    Attacker Class
    Created the attacker Class

---
+++ Tank/attacker.cpp
@@ -0,0 +1,28 @@
+#include "attacker.h"
+#include <stdlib.h>
+#include <QTimer>
+
+Attacker::Attacker()
+{
+    //setting the size
+    setRect(0,0,100,100);
+
+    //setting the position
+    int randomPosition = rand() %500;
+    setPos(0,randomPosition);
+
+    QTimer *timer = new QTimer();
+    connect(timer,SIGNAL(timeout()),this,SLOT(moveAttacker()));
+    timer->start(50);
+
+}
+
+void Attacker::moveAttacker()
+{
+    setPos(x()-5,y());
+
+   /* if(pos().x()>900){
+        scene()->removeItem(this);
+        delete this;
+   */
+}
